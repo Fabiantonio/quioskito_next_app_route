fa799d0a740b1980b73dc0cb74232d41b42caaf0
Fabian Casas <fa.casas@duocuc.cl>
2025-12-01 15:35:02 -0300

    schema.prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "res.cloudinary.com",
-        }],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./node_modules/.prisma/client/**/*"],
     },
+  },
 };
 
 export default nextConfig;
